a4b763ef74b525c0aaa00933a7fbeb9aa7888444
ST10444782 <161002155+ST10444782@users.noreply.github.com>
2024-05-06 19:41:06 +0200

    IMAD Assignment2

--- app/src/main/java/com/example/tamagotchiapp1/MainActivity.kt
+++ app/src/main/java/com/example/tamagotchiapp1/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+    @SuppressLint("WrongViewCast", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
